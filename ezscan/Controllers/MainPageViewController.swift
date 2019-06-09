552e00eca7f432734f940ad219627070546e5f61
Deanna <deannajs@umich.edu>
2019-06-09 11:44:46 -0400

    i'd tap that keyboard away

--- ezscan/Controllers/MainPageViewController.swift
+++ ezscan/Controllers/MainPageViewController.swift
@@ -11,6 +11,8 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
     var imgArray = [UIImage]()
     var selectedImage: UIImage?
 
+
+    
     override func viewDidAppear(_ animated: Bool)
     {
         dataArray = [String]()
@@ -108,5 +110,19 @@ class MainPageViewController: UIViewController, UITableViewDelegate, UITableView
         }
     }
     
+}
+
+
+// Put this piece of code anywhere you like
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
 
